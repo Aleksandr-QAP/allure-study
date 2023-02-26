ac1326eca486afba26265f4a60a1143c6083c8e8
Aleksandr-QAP <alzuzman@gmail.com>
2023-02-26 20:06:23 +0300

    new test

---
+++ tests/test_another_api.py
@@ -0,0 +1,12 @@
+import requests
+import pytest
+import allure
+
+
+@allure.feature('get status_code')
+@allure.story('Пингуем ресурс')
+def test_get_locations_for_us_90210_check_status_code_equals_200():
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    with allure.step("Запрос отправлен, посмотрим код ответа"):
+        assert response.status_code == 200, f"Получен код ответа {response.status_code}"
+
